a4f0bcad23c65e48299005d66437636fb7d612b9
Dant3210 <daneel1982@gmail.com>
2022-11-03 23:04:02 -0400

    modified:   contracts/UglyUrbanGoblin.sol

--- contracts/UglyUrbanGoblin.sol
+++ contracts/UglyUrbanGoblin.sol
@@ -8,61 +8,62 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract UglyUrbanGoblin is ERC1155, Ownable, ReentrancyGuard {
   
-  uint256 constant MAX_SUPPLY = 300;
-  uint8 public maxTx = 10;
+  uint8 public maxTx = 50;
   uint8 public maxPerAccount = 20; 
-  uint16 public maxSupply = 100;
+  uint32 constant ART_SUPPLY=1000;
   uint256 public artPrice;
   string public name;
   string public symbol;
 
-  mapping(uint => string) public tokenURI;
-//uint id_, string memory uri_, uint artPrice_
-  constructor() ERC1155("") {
-    name = "UGLY URBAN GOLBLIN";
-    symbol = "UUG";
-    artPrice=50000000000000000;
-    //setURI(id_,uri_);
-    //tokenURI[id_] = uri_;
-    //_mint(_msgSender(), id_, 100, "");
+  // ID+QTY=>Total Supply per ID
+  mapping(uint => uint)private artSypply;
+  // ID+URI=> TOken URI per ID
+  mapping(uint => string) private tokenURI;
+
+
+  constructor(string memory name_, string memory symbol_, uint256 artPrice_) ERC1155("") {
+    name = name_;//"UGLY URBAN GOLBLIN";
+    symbol = symbol_;//"UUG";
+    artPrice=artPrice_;//50000000000000000;
   } 
 
   receive() external payable {}
 
+  
   function mint(address _to, uint id, uint amount) external {
-    uint256 currentTx=balanceOf(_to,id);
-    require(amount > 0 && amount <= maxPerAccount, "Purchase: amount prohibited");
-    uint256 _supply=currentTx+amount;
-    require(_supply<=maxPerAccount, "Purchase: Max supply reached");
+    require(amount > 0 && amount <= maxPerAccount, "Mint: amount/Tx prohibited");
+    require(balanceOf(_to,id)+amount<=maxPerAccount, "Mint: Max supply/Tx reached");
+    require(artSypply[id]<ART_SUPPLY, "Mint: Mint max supply reached");
     //require(msg.value>=artPrice*amount, "Purchase: Needs more funds");    
     _mint(_to, id, amount, "");
+    artSypply[id]+=amount;
   }
 
-  /*function mintBatch(address _to, uint[] memory ids, uint[] memory amounts) external onlyOwner {
+  function mintBatch(address _to, uint[] memory ids, uint[] memory amounts) external onlyOwner {
         for(uint256 i = 0; i < ids.length; i++){
             require(amounts.length == ids.length, "MintBatch: amounts and ids length mismatch");
-            require(amounts[i] > 0 && amounts[i] <= maxTx, "MintBatch: amount prohibited");
+            require(amounts[i] > 0 && amounts[i] <= maxTx, "MintBatch: amount/Tx prohibited");
 
             uint256 id=ids[i];
-            uint256 _idSupply=balanceOf(_to,id);
-            uint256 _Totalsupply=_idSupply+amounts[i];
-            
-            require(_Totalsupply<=maxTx && amounts[id]<=maxTx, "MintBatch: Max supply reached");
+            require(artSypply[id]<ART_SUPPLY, "MintBatch: Mint max supply reached");
+            require(balanceOf(_to,id)+amounts[i]<=maxTx && amounts[id]<=maxTx, "MintBatch: Max/Tx supply reached");
 
-            //updateSupply(id, amounts[i],msg.sender);
-
+            artSypply[id]+=amounts[i];
         }    
     _mintBatch(_to, ids, amounts, "");
-  }*/
+  }
 
   function burn(uint _id, uint _amount) external {
     require(balanceOf(_msgSender(),_id)>0,"BURN You're not an Owner");
     _burn(msg.sender, _id, _amount);
   }
 
- /* function burnBatch(uint[] memory _ids, uint[] memory _amounts) external onlyOwner{
-    _burnBatch(msg.sender, _ids, _amounts);
-  }*/
+  function burnBatch(uint[] memory _ids, uint[] memory _amounts) external{
+    for(uint256 i; i>=_ids.length; i++){
+      require(balanceOf(_msgSender(),_ids[i])>0,"BATCH_BURN You're not an Owner");
+      _burnBatch(msg.sender, _ids, _amounts);
+    }
+  }
 
   function setURI(uint _id, string memory _uri) external onlyOwner {
     tokenURI[_id] = _uri;
@@ -73,9 +74,9 @@ contract UglyUrbanGoblin is ERC1155, Ownable, ReentrancyGuard {
     return tokenURI[_id];
   }*/
     
-  /*function updateArtPrice(uint256 _newPrice)public onlyOwner{
+  function updateArtPrice(uint256 _newPrice)public onlyOwner{
     artPrice=_newPrice;
-  }   */    
+  }  
 
   function withdraw() public payable onlyOwner nonReentrant(){
     require(payable(_msgSender()).send(address(this).balance));
