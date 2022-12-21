2c4615bc742dc0d51a846c715d898def378bef20
Dant3210 <daneel1982@gmail.com>
2022-12-20 23:16:38 -0500

    modified:   contracts/UglyUrbanGoblin.sol

--- contracts/UglyUrbanGoblin.sol
+++ contracts/UglyUrbanGoblin.sol
@@ -7,13 +7,14 @@ import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/common/ERC2981Upgradeable.sol";
 
 
-contract UglyUrbanGoblin is Initializable, UUPSUpgradeable, ERC1155Upgradeable, OwnableUpgradeable, ReentrancyGuardUpgradeable {
+contract UglyUrbanGoblin is Initializable, UUPSUpgradeable, ERC1155Upgradeable,ERC2981Upgradeable, OwnableUpgradeable, ReentrancyGuardUpgradeable {
   
 
-  /*uint256  public MAX_PER_ACCT; 
-  uint256  public ART_SUPPLY;*/
+  /*uint256  public MAX_PER_ACCT; */
+  uint256  public constant ART_SUPPLY=100;
   uint256 private artPrice;
   string private name;
   string private symbol;
@@ -29,11 +30,13 @@ contract UglyUrbanGoblin is Initializable, UUPSUpgradeable, ERC1155Upgradeable, 
     name=name_;
     symbol=symbol_;
     paySplitter = payable (_payment);
+    
 
     __Ownable_init();
     __UUPSUpgradeable_init();
     __ReentrancyGuard_init();
-    __ERC1155_init("");    
+    __ERC1155_init("");   
+    __ERC2981_init();  
   }
 
   ///@dev required by the OZ UUPS module
@@ -41,8 +44,8 @@ contract UglyUrbanGoblin is Initializable, UUPSUpgradeable, ERC1155Upgradeable, 
   
   function mint(address _to, uint id, uint amount) external payable nonReentrant{
     require(amount > 0, "Mint: amount/Tx prohibited");
-    require(balanceOf(_to,id)+amount <= 100, "Mint: Supply/Tx reached");
-    require(artSypply[id]<=1000, "Mint: Art max supply reached");
+    require(artSypply[id]<=ART_SUPPLY, "Mint: Art max supply reached");
+    require(balanceOf(_to,id)+amount<6, "Mint: Supply/Tx reached");
     require(msg.value>=artPrice*amount, "Mint: Needs more funds"); 
      artSypply[id]+=amount;   
     _mint(_to, id, amount, "");
@@ -52,13 +55,9 @@ contract UglyUrbanGoblin is Initializable, UUPSUpgradeable, ERC1155Upgradeable, 
   function mintBatch(address _to, uint[] memory ids, uint[] memory amounts) external onlyOwner {
         for(uint256 i = 0; i < ids.length; i++){
             require(amounts.length == ids.length, "MintBatch: amounts and ids length mismatch");
-            require(amounts[i] > 0 && amounts[i]<101, "MintBatch: amount/Tx prohibited");
-
+            require(amounts[i] > 0 && amounts[i]<=ART_SUPPLY, "MintBatch: amount/Tx prohibited");
             uint256 id=ids[i];
-            require(artSypply[id]<1000, "MintBatch: Art max supply reached");
-            //uint balance=balanceOf(_to,id);
-            //require(balance<=maxTx && amounts[id]<=maxTx, "MintBatch: Max/Tx reached");
-
+            require(artSypply[id]<=ART_SUPPLY, "MintBatch: Art max supply reached");
             artSypply[id]+=amounts[i];
         }    
     _mintBatch(_to, ids, amounts, "");
@@ -104,6 +103,14 @@ contract UglyUrbanGoblin is Initializable, UUPSUpgradeable, ERC1155Upgradeable, 
   function getSymbol()public view returns(string memory){
     return symbol;
   }
+
+  // EIP2981 standard Interface return. Adds to ERC1155 and ERC165 Interface returns.
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC1155Upgradeable,ERC2981Upgradeable) returns (bool){
+        return (
+            interfaceId == type(ERC2981Upgradeable).interfaceId ||
+            super.supportsInterface(interfaceId)
+        );
+    }
   
   function transferFunds(uint256 balance) private{ 
     require(payable(paySplitter).send(balance));
